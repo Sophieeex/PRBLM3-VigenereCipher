4f71b897de6a7054653e61711f9a90ca0977e02f
Your Name <you@example.com>
2023-05-23 08:51:11 +0800

    Pseudocode

---
+++ vigenereCipher.py
@@ -0,0 +1,11 @@
+# Name: Sophia Marie T. Reyes
+# Year & Section: BSCpE 1-5
+# Laboratory Program: Problem 3 - The Vigenere Cipher
+
+# create a function that provides a value for each character 
+# from the inputted text to generate an encrypted message
+
+# formats the key based on the length of the inputted text
+
+# the user type the message and key
+# the output printed is based on the condition met
